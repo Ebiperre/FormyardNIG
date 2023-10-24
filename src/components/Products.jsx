8a4ece630a9017fc9fb4ba11ccad1c1de896951e
Ezeoye Goodluck <116758130+EzeoyeGoodluck@users.noreply.github.com>
2023-10-24 09:14:58 -0700

    product section

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -20,11 +20,10 @@ const Services = () => {
           ornatus nam ei.
         </p>
       </div>
-      <div className="grid grid-cols-4 gap-[23px] mt-8 ">
+      <div className="grid grid-cols-4 gap-[23px] mt-4 ">
         {data.map((stat) => (
-          <div className="flex flex-col place-content-center gap-[32px]    w-fit  ">
-            <div></div>
-            <div className="flex  items-center justify-center gap-4 w-[240px] h-[78px]  rounded-full  border border-black py-4 px-4 bg-red-100 ">
+          <div className="flex flex-col place-content-center gap-[32px]    w-fit">
+            <div className="flex  items-center justify-center gap-4 w-[240px] h-[78px]  rounded-full  border border-black py-4 px-4 b">
               <p className="text-base text-center">{stat.text}</p>
             </div>
           </div>
