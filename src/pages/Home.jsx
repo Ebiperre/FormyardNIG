6550eeac7a121f2c641ae300e61e8b57e2f7d4a3
Ezeoye Goodluck <116758130+EzeoyeGoodluck@users.noreply.github.com>
2023-10-24 09:09:18 -0700

    product page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navigation from '../components/Navigation'
+// import Navigation from '../components/Navigation'
 import Hero from '../components/Hero'
 import Stats from '../components/Stats'
 import Services from '../components/Services'
