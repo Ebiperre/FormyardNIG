8d565a2154cfa7156a1d32471fc07d2646355a87
Ebiperre Iyoro <iyorop@yahoo.com>
2023-10-24 11:32:30 +0100

    quick fix

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Portfolio from './pages/Portfolio';
 import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
