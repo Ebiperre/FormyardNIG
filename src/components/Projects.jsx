7f85bba6823790cf221c5d5d486c464a2a334d70
Ebiperre Iyoro <iyorop@yahoo.com>
2023-11-08 06:51:23 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Projects = () => {
   return (
@@ -7,7 +8,10 @@ const Projects = () => {
             <h2 className='lg:bold-56 bold-28 lg:leading-[64px] text-center lg:text-left'>Have any Upcoming Project?</h2>
             <p className='w-full text-center lg:text-left'>We bring your ideas to life with quality craftsmanship.</p>
             <button className='rounded-full text-black text-base font-bold lg:text-start bg-[#FFDD00] px-12  py-3 '>
+                <Link to="/contact">
                 Get Started
+                </Link>
+                
             </button>
 
         </div>
