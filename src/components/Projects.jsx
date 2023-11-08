843949773005a7f0572d08c77fc26a4f4fed8e40
Ebiperre Iyoro <iyorop@yahoo.com>
2023-11-08 06:52:36 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Projects = () => {
   return (
-    <section className='2xl:max-container 4xl:max-none  relative flex flex-col px-8 lg:px-20 py-10 lg:mb-10 lg:py-20 xl:mb-20'>
-        <div className='bg-[#EAF3F5] rounded-xl p-3 pb-12 space-y-4 text-center lg:text-left'>
+    <section className='2xl:max-container 4xl:max-none  relative flex flex-col px-8 lg:px-20 py-10 lg:mb-10 lg:py-10 xl:mb-20'>
+        <div className='bg-[#EAF3F5] rounded-xl p-5 pb-12 space-y-4 text-center lg:text-left'>
             <h2 className='lg:bold-56 bold-28 lg:leading-[64px] text-center lg:text-left'>Have any Upcoming Project?</h2>
             <p className='w-full text-center lg:text-left'>We bring your ideas to life with quality craftsmanship.</p>
             <button className='rounded-full text-black text-base font-bold lg:text-start bg-[#FFDD00] px-12  py-3 '>
